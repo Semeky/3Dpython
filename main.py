df1ce75a4b7fbd4e20b6fa9e9b1d821f6f9e46d1
simon <sim386007@gmail.com>
2024-08-27 13:02:37 +0300

    player movment

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame
 from settings import *
+from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+player = Player()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-
+    player.movement
     screen.fill(BLACK)
 
-    pygame.draw.circle(screen, GREEN, player_pos, 12)
+    pygame.draw.circle(screen, GREEN, player.pos, 12)
 
     pygame.display.flip()
-    clock.tick()
+    clock.tick(FPS)
--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ WIDTH = 1200
 HEIGHT = 800
 HALF_WIDTH = WIDTH // 2
 HALF_HEIGHT = HEIGHT // 2
+FPS = 60
 
 player_pos = (HALF_WIDTH, HALF_HEIGHT)
 player_angle = 0
